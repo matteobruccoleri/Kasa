e7655b2c7a3c2328b6a3e4e7a87eafc0f4bf2859
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-11-27 23:14:22 +0100

    ajout component RatingStars

---
+++ src/components/atoms/RatingStars/RatingStars.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./RatingStars.module.scss";
+import StarActive from "../../../assets/images/star-active.svg"
+import StarInactive from "../../../assets/images/star-inactive.svg"
+
+const RatingStars = ({ rating, totalStars = 5 }) => {
+    // Génération des étoiles en fonction du rating
+    const generateStars = () => {
+        const stars = [];
+        for (let i = 1; i <= totalStars; i++) {
+            stars.push(
+                <img
+                    key={i}
+                    src={i <= rating ? StarActive : StarInactive}
+                    alt={i <= rating ? "Étoile active" : "Étoile inactive"}
+                    className={styles.star}
+                />
+            );
+        }
+        return stars;
+    };
+
+    return <div className={styles.ratingWrapper}>{generateStars()}</div>;
+};
+
+// Définition des types de props
+RatingStars.propTypes = {
+    rating: PropTypes.number.isRequired, // Note à afficher
+    totalStars: PropTypes.number,        // Nombre total d'étoiles (optionnel, par défaut 5)
+};
+
+export default RatingStars;

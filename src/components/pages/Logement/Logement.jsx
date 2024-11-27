548180f90273e4a82b4275e11e6b8cc37ddd70cd
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-11-27 23:15:45 +0100

    feat: réutilisation du component RatingStars pour affiché la note du logement

--- src/components/pages/Logement/Logement.jsx
+++ src/components/pages/Logement/Logement.jsx
@@ -2,7 +2,7 @@ import { useParams, Navigate } from "react-router-dom";
 import Logements from "../../../data/logements.js";
 import styles from "./Logement.module.scss";
 import Tags from "../../molecules/Tags/Tags.jsx";
-
+import RatingStars from "../../atoms/RatingStars/RatingStars.jsx"; // Import du composant
 
 function Logement() {
     const { id } = useParams(); // Récupère l'ID comme une chaîne de caractères
@@ -16,19 +16,33 @@ function Logement() {
 
     return (
         <main className={styles.logement}>
-            <img src={logement.cover} alt={logement.title} />
+            <div className={styles.carrousel}>
+                <img src={logement.cover} alt={logement.title} />
+            </div>
             <div className={styles.fiche_wrapper}>
                 <div className={styles.title_wrapper}>
                     <h1>{logement.title}</h1>
                     <p>{logement.location}</p>
+                    <Tags tags={logement.tags} />
                 </div>
-                <div className={styles.name_wrapper}>
-                    <p>{logement.host.name}</p>
-                    <p>{logement.host.picture}</p>
+                <div className={styles.host_wrapper}>
+                    <div className={styles.profil_wrapper}>
+                        <p>{logement.host.name}</p>
+                        <img src={logement.host.picture} alt={logement.title} />
+                    </div>
+                    <div className={styles.rating_wrapper}>
+                            <RatingStars rating={logement.rating} />
+                    </div>
                 </div>
             </div>
-            <Tags tags={logement.tags} />
-            <p>{logement.description}</p>
+            <div className={styles.dropdown_wrapper}>
+                <div>
+                    <p>{logement.description}</p>
+                </div>
+                <div>
+                    <p>{logement.equipments}</p>
+                </div>
+            </div>
         </main>
     );
 }

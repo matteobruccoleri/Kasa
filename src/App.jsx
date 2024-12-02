c633d9ee74aee2b61312aaef73eef3e51472e7fb
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-12-02 22:33:29 +0100

    feat: maj App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.module.scss';
+//import './App.module.scss';
 import './assets/styles/global.module.scss';
 
 import Footer from './components/organisms/Footer/Footer';
